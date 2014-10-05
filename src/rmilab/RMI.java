66895bf9f4796daf43b13dfb3bb8a3bbe45b1829
iamfrodo <nandini.rk94@gmail.com>
2014-10-04 23:49:30 -0400

    Began writing main loop of Server-side

--- src/rmilab/RMI.java
+++ src/rmilab/RMI.java
@@ -1,9 +1,15 @@
 package rmilab;
 
 import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.net.InetAddress;
 import java.net.ServerSocket;
+import java.net.Socket;
 
+import edu.cmu.cs.cs440.hw1.MigratableProcess;
+
+
+/* This is the server in our RMI Application */
 public class RMI {
 	static String host;
 	static int port;
@@ -14,9 +20,8 @@ public class RMI {
 		int registryPort = Integer.parseInt(args[2]);	
 		String serviceName = args[3];		/* What is the service name? the method name? */
 
-		// it should have its own port. assume you hardwire it.
-		host = (InetAddress.getLocalHost()).getHostName();
-		port = 12345;
+		host = (InetAddress.getLocalHost()).getHostName();	/* Host name of server */
+		port = 12345;	/* Port of the server. Hard-coded */
 
 		Class initialclass = Class.forName(InitialClassName);
 		Class initialskeleton = Class.forName(InitialClassName+"_skel");
@@ -26,6 +31,8 @@ public class RMI {
 		// as a hint, I give such a table's interface as RORtbl.java. 
 		RORTable tbl = new RORTable();
 
+		// DO A LOOK UP HERE? RETURN THE LOOKUP TO THE SERVER
+
 		// after that, you create one remote object of fibonacci.
 		Object o = initialclass.newInstance(); /* new Fibonacci() */
 
@@ -33,9 +40,39 @@ public class RMI {
 		//tbl.addObj(host, port, o);
 		// IS THIS THE SAME AS BINDING?
 
-		tbl.bind(1, (RemoteObjRef) o);
+		//tbl.bind(1, (RemoteObjRef) o);
 
 		// create a socket.
 		ServerSocket serverSoc = new ServerSocket(port);
+		while (true)
+		{
+			Socket clientSocket = null;
+			try {
+				clientSocket = serverSoc.accept();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			ObjectInputStream in;
+			in = new ObjectInputStream(clientSocket.getInputStream());
+			int objKey = (Integer) in.readObject();
+			RemoteObjRef ror = tbl.lookup(objKey);
+
+			// (1) receives an invocation request.
+			// (2) creates a socket and input/output streams.
+			// (3) gets the invocation, in martiallled form.
+			// (4) gets the real object reference from tbl.
+			// (5) Either:
+			//      -- using the interface name, asks the skeleton,
+			//         together with the object reference, to unmartial
+			//         and invoke the real object.
+			//      -- or do unmarshalling directly and involkes that
+			//         object directly.
+			// (6) receives the return value, which (if not marshalled
+			//     you should marshal it here) and send it out to the 
+			//     the source of the invoker.
+			// (7) closes the socket.
+			clientSocket.close();
+		}
 	}
 }
